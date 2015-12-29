1cd3fc28382cbffcd862027b26e98edcda5ff27e
Daniel Parks <dp@puppetlabs.com>
2015-12-29 02:56:45 -0800

    Certs and keys shouldn't be accessible to anyone but root.

--- manifests/resource/vhost.pp
+++ manifests/resource/vhost.pp
@@ -602,33 +602,38 @@ define nginx::resource::vhost (
     # Check if the file has been defined before creating the file to
     # avoid the error when using wildcard cert on the multiple vhosts
     ensure_resource('file', "${nginx::config::conf_dir}/${cert}.crt", {
-      owner  => $nginx::config::daemon_user,
+      owner  => 'root',
+      group  => '0',
       mode   => '0444',
       source => $ssl_cert,
     })
     ensure_resource('file', "${nginx::config::conf_dir}/${cert}.key", {
-      owner  => $nginx::config::daemon_user,
-      mode   => '0440',
+      owner  => 'root',
+      group  => '0',
+      mode   => '0400',
       source => $ssl_key,
     })
     if ($ssl_dhparam != undef) {
       ensure_resource('file', "${nginx::config::conf_dir}/${cert}.dh.pem", {
-        owner  => $nginx::config::daemon_user,
-        mode   => '0440',
+        owner  => 'root',
+        group  => '0',
+        mode   => '0400',
         source => $ssl_dhparam,
       })
     }
     if ($ssl_stapling_file != undef) {
       ensure_resource('file', "${nginx::config::conf_dir}/${cert}.ocsp.resp", {
-        owner  => $nginx::config::daemon_user,
-        mode   => '0440',
+        owner  => 'root',
+        group  => '0',
+        mode   => '0400',
         source => $ssl_stapling_file,
       })
     }
     if ($ssl_trusted_cert != undef) {
       ensure_resource('file', "${nginx::config::conf_dir}/${cert}.trusted.crt", {
-        owner  => $nginx::config::daemon_user,
-        mode   => '0440',
+        owner  => 'root',
+        group  => '0',
+        mode   => '0400',
         source => $ssl_trusted_cert,
       })
     }
